dd47091bd592b25adde0fa8ff9a0fa156e4bb454
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-08-24 12:56:49 +0530

    index.php code updated

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$request = Illuminate\Http\Request::capture();
-
-$response = $app->handleRequest($request);
-
-
+$app->handleRequest(Request::capture());
