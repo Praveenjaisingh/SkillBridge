784c6a9bf104e5a2388d56521afa373386476682
Praveen <dpp469926@gmail.com>
2026-08-25 13:20:26 +0530

    smtp issue fixed

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
     {
         Auth::guard('web')->logout();
         $request->session()->invalidate();
-        $request->session()->regenerateToken();
+        $request->session()->regenerateToken(); 
         return redirect('/');
     }
 }
